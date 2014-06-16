f85eecb6496f5445673c249f957e1aa1ad2683ae
hiroshi kobayashi <koba.shipbuilding@gmail.com>
2014-06-16 18:08:05 +0900

    add icon. cache.manifest

--- js/kanji.js
+++ js/kanji.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var storage_key = 'kanji';
+  var storage_key = 'kanji.0.2';
   var input = $('#kanji');
   var KEY = function(){};
   KEY.ENTER = 13;
@@ -13,39 +13,53 @@ $(document).ready(function(){
   }
 
   // initialize
-  if(loadStorage() == undefined ) saveStorage({});
+  if(loadStorage() == undefined) saveStorage({});
 
   var history = loadStorage();
 
   function print_history(){
     $('#hist').html('');
-    sortable = [];
-    $.each(history, function(kanji, count) {
-      sortable.unshift({kanji: kanji, count: count})
-    });
-    $.each(sortable.sort(function(a, b){
-      return b.count - a.count;
-    }), function(i, o){
-      size = 18 + o.count * 2;
-      $('#hist').append('<li class="btn" style="font-size:'+size+'pt;">'+o.kanji+'</li>');
+    var sort_by_count      = function(a, b){ return b.count - a.count; }
+    var sort_by_created_at = function(a, b){ return b.updated_at - a.updated_at; }
+    var array = []
+
+    // hash to array
+    $.each(history, function(key, h){ array.unshift(h); });
+    $.each(array.sort(sort_by_created_at), function(index, h){
+      append(h);
     });
   }
 
+  function append(hist){
+    size = 20 + hist.count * 4;
+    var li = 
+      '<li class="" style="font-size:'+size+'pt;">' +
+        hist.word + 
+      '</li>';
+    $('#hist').append(li);
+  }
+
+  function clean_input(){
+    return $('<div>').text(input.val()).html(); // excape
+  }
+
   function save(){
-    str = $('<div>').text(input.val()).html(); // excape
+    str = clean_input();
     if(str == '') return;
     input.val('');
+
     if(history[str]){
-      history[str] += 1;
+      history[str]['count'] += 1;
+      history[str]['updated_at'] = Date.now();
     }else{
-      history[str] = 1;
+      history[str] = { count: 1, word: str, updated_at: Date.now() };
     }
     saveStorage(history);
     print_history();
   }
 
   function fitFontSizeToBox(box_id, inner_id){
-    var box_width = $(box_id).width();
+    var box_width = $(box_id).width() - 20;
     var font_size = 20;
     while($(inner_id).width() + 20 < box_width){
       font_size += 3;
@@ -68,6 +82,22 @@ $(document).ready(function(){
     save();
   });
 
+  var type_at = Date.now();
+  var timer = 0;
+  input.keyup(function(){
+    if(timer > 0) clearInterval(timer);
+    timer = setTimeout(function(){
+      var str = clean_input();
+      if(str == '' || str.match(/^[\u3040-\u309F\u30A0-\u30FFa-zA-Z0-9]+$/)){
+        // ignore hiragana, katakana, alpha, number
+      }else{
+        save();
+        input.val('');
+      }
+      timer = 0;
+    }, 5000);
+  });
+
   $('#delete-button').click(function(){
     var str = $('#loupe-text').html();
     delete history[str];
@@ -82,10 +112,37 @@ $(document).ready(function(){
   });
 
   $('#hist').delegate('li', 'click', function(){
-    $('#loupe-text').text($(this).text());
+    var str = $(this).text();
+    var h = history[str];
+    $('#loupe-text').text(str);
+    $('#loupe-count').html( h.count + "回");
+    $('#loupe-updated_at').text(date_for(h.updated_at));
+    var top = $(window).scrollTop();
+    var margin_top =  top + 'px'
+    $('#loupe').css('margin-top', margin_top );
     $('#loupe').slideDown();
     fitFontSizeToBox('#loupe', '#loupe-text');
   });
+
+  function date_for(date_obj){
+    var d = new Date(date_obj);
+    return [
+      d.getFullYear(),
+      pad(d.getMonth() + 1),
+      pad(d.getDate()),
+    ].join("-") + " " + [
+      pad(d.getHours()),
+      pad(d.getMinutes())
+    ].join(":")
+  }
+
+  function pad(num){
+    if(num < 10){
+      return "0" + num;
+    }else{
+      return num;
+    }
+  }
 
   $('#loupe').click(function(){
     $(this).slideUp('fast', function(){
